9dafd832330fa56695a3cee899d66dcfd73cb4ee
unknown <hempghans@gmail.com>
2021-06-24 20:34:46 -0500

    modificación

--- SistemaClinico_DC/src/sistemaclinico_dc/Cita.java
+++ SistemaClinico_DC/src/sistemaclinico_dc/Cita.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *comentario
  */
 package sistemaclinico_dc;
 
@@ -16,6 +17,7 @@ public class Cita {
     protected Date fecha;
     protected boolean pagada;
     protected String registradoPor;
+    
     
     public void realizarPago(Pago pago){
             }
